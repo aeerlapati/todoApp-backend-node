5e17000cd94975455c1ee17ae192a4120784dd47
Santhoshi Jaini <sjaini@Santhoshis-MacBook-Air.local>
2025-02-13 17:34:04 -0700

    Updated Folder Names

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { handleWriteResponse, handleGetResponse } = require('./data/posts');
+const { handleWriteResponse, handleGetResponse } = require('./utils/posts');
 
 const app = express();
 
